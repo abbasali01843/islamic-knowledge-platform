63d8386c230e40f21fd84d94f43e2f5fef8b1fd4
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-10 01:49:39 +0600

    build: define modular Android project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,46 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IslamicKnowledgePlatform"
+include(":app")
+include(":core:common")
+include(":core:model")
+include(":core:database")
+include(":core:network")
+include(":core:navigation")
+include(":core:search")
+include(":core:audio")
+include(":core:location")
+include(":core:notifications")
+include(":core:design")
+include(":feature:home")
+include(":feature:quran")
+include(":feature:hadith")
+include(":feature:prayer")
+include(":feature:ramadan")
+include(":feature:qibla")
+include(":feature:dua")
+include(":feature:athkar")
+include(":feature:learn")
+include(":feature:seerah")
+include(":feature:calendar")
+include(":feature:zakat")
+include(":feature:hajj")
+include(":feature:quiz")
+include(":data:quran")
+include(":data:hadith")
+include(":data:dua")
+include(":data:knowledge")
