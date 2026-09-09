a5f305f8d030c2d48e72a80b2f2c7f99d23deac1
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-10 01:55:45 +0600

    feat(quran): add core model module

---
+++ core/model/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.islamicknowledge.platform.core.model"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+}
