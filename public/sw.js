316422c4dfc699c2f6bb7d5f6b5d793f2d84415e
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-19 21:50:18 +0600

    Remove offline service worker

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,1 @@
-const CACHE='ikp-web-v1';
-const APP_SHELL=['/','/manifest.webmanifest','/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  const url=new URL(e.request.url);
-  if(url.origin!==location.origin) return;
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match('/'))));
-});
+Remove offline service worker
