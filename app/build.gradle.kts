1cd345b6e1ee889356f61bea594b8155d1132436
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-19 20:30:55 +0600

    Fix Android feature dependencies placement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,8 +55,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     debugImplementation("androidx.compose.ui:ui-tooling")
-}
-
     implementation(project(":feature:ramadan"))
     implementation(project(":feature:hajj"))
     implementation(project(":feature:seerah"))
+}
