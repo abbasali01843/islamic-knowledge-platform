84bd4df86a9901a0ca52b8d622f2cc0125c93977
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-19 20:25:12 +0600

    Add Qibla app dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(project(":feature:prayer"))
     implementation(project(":feature:hadith"))
     implementation(project(":feature:dua"))
+    implementation(project(":feature:qibla"))
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.compose.ui:ui")
