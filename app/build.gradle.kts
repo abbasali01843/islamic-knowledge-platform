bd8dda613abb289b415f85a4e7ff43438a672ab3
Abbas Ali <abbasalictg01815@gmail.com>
2026-09-19 20:27:46 +0600

    Add Learn Salah dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(project(":feature:qibla"))
     implementation(project(":feature:calendar"))
     implementation(project(":feature:zakat"))
+    implementation(project(":feature:learn"))
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.10.0")
     implementation("androidx.compose.ui:ui")
